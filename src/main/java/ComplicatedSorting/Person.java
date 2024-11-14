2dc9b14386fcfae05985be47cfea78b4406a7165
Nagarajan Sundaram <nagasundar@gmail.com>
2024-11-13 19:44:39 -0500

    Add java 8 feature examples

--- src/main/java/ComplicatedSorting/Person.java
+++ src/main/java/ComplicatedSorting/Person.java
@@ -24,23 +24,12 @@ public class Person {
         return name;
     }
 
-    public void setName(String name) {
-        this.name = name;
-    }
-
     public int getAge() {
         return age;
-    }
-
-    public void setAge(int age) {
-        this.age = age;
     }
 
     public String getCity() {
         return city;
     }
 
-    public void setCity(String city) {
-        this.city = city;
-    }
 }
--- src/main/java/ch5/CreateStreams.java
+++ src/main/java/ch5/CreateStreams.java
@@ -8,19 +8,25 @@ import java.util.stream.Stream;
 
 public class CreateStreams {
     public static void main(String[] args) {
-        String[] names = { "Adnane", "Mary", "Laila", "Dennis", "Nitin", "Sairam", "Maaike" };
+        String[] names = { "Adnan", "Mary", "Laila", "Dennis", "Nitin", "Sairam", "Maaike" };
         List<String> nameList = new ArrayList<>(Arrays.asList(names));
 
         // create stream based on array
         Stream<String> arrayStream = Arrays.stream(names);
 
+        // filter
+        arrayStream.filter(s -> s.startsWith("M")).forEach(System.out::println);
         // create stream based on collection
         Stream<String> listStream = nameList.stream();
 
+        // map
+        listStream.map(s -> s + " student").forEach(System.out::println);
+
         // create stream using generate method
         Stream<Integer> generateStream = Stream.generate(() -> 5);
+        generateStream.limit(5).forEach(System.out::println);
         // create stream using iterate method
         Stream<Integer> iterateStream = Stream.iterate(1, x -> x + 2);
-
+        iterateStream.limit(5).forEach(System.out::println);
     }
 }
